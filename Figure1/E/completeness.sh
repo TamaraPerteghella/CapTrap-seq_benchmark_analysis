f8b02c7918f1017c612c1d3360977dc5dc8eb3c2
TamaraPerteghella <25196661+TamaraPerteghella@users.noreply.github.com>
2024-05-15 12:08:12 +0200

    Update completeness.sh

--- Figure1/E/completeness.sh
+++ Figure1/E/completeness.sh
@@ -1,13 +1,13 @@
 while read lab cap tmp sample
 do
-    polya="./polya/${lab}_${cap}_${tmp}_${sample}.polyAsites.bed.gz"
-    polyafilt="./polyafilter/${lab}_${cap}_${tmp}_${sample}.polyAreads.list"
+    polya="../../data/polya/${lab}_${cap}_${tmp}_${sample}.polyAsites.bed.gz"
+    polyafilt="../../data/polya/polyafilter/${lab}_${cap}_${tmp}_${sample}.polyAreads.list"
         
     zgrep -Ff $polyafilt $polya > ${lab}_${cap}_${tmp}_${sample}.polyAreads.refined.list
-    original="./bed/${lab}_${cap}_${tmp}_${sample}.bed.gz"        
+    original="../../data/bed/${lab}_${cap}_${tmp}_${sample}.bed.gz"        
     zegrep -w "SIRVome_isoforms|ERCC" $original > ${lab}_${cap}_${tmp}_${sample}.sirvs.bed
 
-    cage="${capture}_CAGE_peaks.bed"
+    cage="../../data/human_CAGE_peaks.bed"
     zcat $original | awk -F"\t" -v OFS="\t" '{ if( $6 == "+" ) print $1, $2-50, $2+50, $4, "0", $6; else print $1, $3-50, $3+50, $4, "0", $6 }' | awk -F"\t" -v OFS="\t" '{ if( $2 < 0 ) print $1, "0", $3, $4, $5, $6; else print $0 }' > ${lab}_${cap}_${tmp}_${sample}.starts.bed
     bedtools intersect -u -s -a ${lab}_${cap}_${tmp}_${sample}.starts.bed -b $cage > ${lab}_${cap}_${tmp}_${sample}.crossCAGE.bed
 
