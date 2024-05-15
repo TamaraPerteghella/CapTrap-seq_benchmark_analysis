c396e850651f0ee783cd25a456a5eb4ce9bd5374
TamaraPerteghella <25196661+TamaraPerteghella@users.noreply.github.com>
2024-05-15 14:34:52 +0200

    Update read_length.sh

--- FigureS1/B/read_length.sh
+++ FigureS1/B/read_length.sh
@@ -1,1 +1,10 @@
+while read lab cap tmp sample
+do
+    path="../../data/gff/${lab}_${cap}_${tmp}_${sample}.gff.gz"
+    details=$(echo -e "$lab\t$cap\t$tmp\t$sample")
+    if [ -f $path ]; then
+        zcat $path | egrep -v "SIRVome_isoforms|ERCC" | awk -F"\t" -v OFS="\t" -v det="$details" '{ split($9, ids, "\""); print $5-$4, ids[2], det }'
+    fi
+done < ../samples.benchmark.supplementary.human.tsv > benchmark.supplementary.readlengths.tsv
 
+awk -F'\t' '{key = "\"" $2 "\"" "\t" $3 "\t" $4 "\t" $5 "\t" $6; sums[key]+=$1} END {for (key in sums) print key "\t" sums[key]}' benchmark.supplementary.readlengths.tsv > benchmark.supplementary.readlength.collate.tsv
