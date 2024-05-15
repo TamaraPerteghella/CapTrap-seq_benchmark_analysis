821e6d57eaba2218ffaab631a0f33c21d5a92372
TamaraPerteghella <25196661+TamaraPerteghella@users.noreply.github.com>
2024-05-15 11:20:50 +0200

    Update read_length.sh

--- Figure1/D/read_length.sh
+++ Figure1/D/read_length.sh
@@ -5,6 +5,6 @@ do
     if [ -f $path ]; then
         zcat $path | egrep -v "SIRVome_isoforms|ERCC" | awk -F"\t" -v OFS="\t" -v det="$details" '{ split($9, ids, "\""); print $5-$4, ids[2], det }'
     fi
-done < samples.benchmark.tsv > benchmark.readlengths.tsv
+done < ../../samples.benchmark.human.tsv > benchmark.readlengths.tsv
 
 awk -F'\t' '{key = "\"" $2 "\"" "\t" $3 "\t" $4 "\t" $5 "\t" $6; sums[key]+=$1} END {for (key in sums) print key "\t" sums[key]}' benchmark.readlengths.tsv > benchmark.readlength.collate.tsv
