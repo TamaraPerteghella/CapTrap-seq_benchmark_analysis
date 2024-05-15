7700c799a93c052b3d4b3b51069852bb99f70f1e
TamaraPerteghella <25196661+TamaraPerteghella@users.noreply.github.com>
2024-05-15 17:57:19 +0200

    Update ercc_count.sh

--- Figure4/C/ercc_count.sh
+++ Figure4/C/ercc_count.sh
@@ -4,12 +4,12 @@ do
     platform=$(echo $lab | awk -F'-' '{print $1}')
         
     if [ -f $file ]; then
-        fgrep -f ../../data/sirvs_mappings/HS.erccSpikein.ids.list $file | while read text cap sf labx chr strt stp ID count
+        fgrep -f ../../data/sirvs_mapping/HS.erccSpikein.ids.list $file | while read text cap sf labx chr strt stp ID count
         do
             echo -e "$sample\t$platform\t$ID\t$count\tERCC\t$lab\t$cap"
         done
 
-        fgrep -f ../../data/sirvs_mappings/HS.notTargeted.erccSpikein.ids.list $file | while read text cap sf labx chr strt stp ID count
+        fgrep -f ../../data/sirvs_mapping/HS.notTargeted.erccSpikein.ids.list $file | while read text cap sf labx chr strt stp ID count
         do
             echo -e "$sample\t$platform\t$ID\t$count\tERCC\t$lab\t$cap"
         done
@@ -18,7 +18,7 @@ done < ../samples.benchmark.sirvs.tsv > human.spikein.read.count.tsv
 
 cat human.spikein.read.count.tsv | while read sample platform ERCC count tmp lab cap 
 do
-    cat ../../data/sirvs_mappings/ERCC.concs | awk -v spike=$ERCC '$2==spike'| while read nb spikeIn subgroup conc1 conc2 FC sth
+    cat ../../data/sirvs_mapping/ERCC.concs | awk -v spike=$ERCC '$2==spike'| while read nb spikeIn subgroup conc1 conc2 FC sth
     do
        echo -e "$sample\t$platform\t$ERCC\t$count\t$conc1\tERCC\t$lab\t$cap"
     done
