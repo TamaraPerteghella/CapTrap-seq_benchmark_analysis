77e27b9d7e30071774b3e7d27a27cdae847df3cf
TamaraPerteghella <25196661+TamaraPerteghella@users.noreply.github.com>
2024-05-15 11:42:18 +0200

    Update completeness.sh

--- Figure1/E/completeness.sh
+++ Figure1/E/completeness.sh
@@ -1,10 +1,10 @@
 while read lab cap tmp sample
 do
-    polya="/users/project/gencode_006070_no_backup/epalumbo/pilot/output/mappings/polyAmapping/${lab}_${cap}_${tmp}_${sample}.polyAsites.bed.gz"
-    polyafilt="/nfs/users/project/gencode_006070_no_backup/epalumbo/pilot/output/mappings/getPolyAreadsList/${lab}_${cap}_${tmp}_${sample}.polyAreads.list"
+    polya="./polya/${lab}_${cap}_${tmp}_${sample}.polyAsites.bed.gz"
+    polyafilt="./polyafilter/${lab}_${cap}_${tmp}_${sample}.polyAreads.list"
         
     zgrep -Ff $polyafilt $polya > ${lab}_${cap}_${tmp}_${sample}.polyAreads.refined.list
-    original="/users/project/gencode_006070_no_backup/epalumbo/pilot/output/mappings/readBamToBed/${lab}_${cap}_${tmp}_${sample}.bed.gz"        
+    original="./bed/${lab}_${cap}_${tmp}_${sample}.bed.gz"        
     zegrep -w "SIRVome_isoforms|ERCC" $original > ${lab}_${cap}_${tmp}_${sample}.sirvs.bed
 
     cage="${capture}_CAGE_peaks.bed"
@@ -15,7 +15,7 @@ do
     if [ $(wc -l ${lab}_${cap}_${tmp}_${sample}.sirvs.bed | cut -c1 ) -eq 0 ]; then
         rm ${lab}_${cap}_${tmp}_${sample}.sirvs.bed
     fi
-    singularity exec ~/docker_images/r4_gencode_phase3_latest.sif Rscript completeness_read_level.R $original ${lab}_${cap}_${tmp}_${sample}.crossCAGE.bed ${lab}_${cap}_${tmp}_${sample}.polyAreads.refined.list ${lab}_${cap}_${tmp}_${sample}.sirvs.bed
+    Rscript completeness_collate.R $original ${lab}_${cap}_${tmp}_${sample}.crossCAGE.bed ${lab}_${cap}_${tmp}_${sample}.polyAreads.refined.list ${lab}_${cap}_${tmp}_${sample}.sirvs.bed
 
 done < ../../samples.benchmark.human.tsv
 
