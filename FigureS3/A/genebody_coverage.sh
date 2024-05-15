889f049190069baf94e10a338630b3315346ab66
TamaraPerteghella <25196661+TamaraPerteghella@users.noreply.github.com>
2024-05-15 17:28:56 +0200

    Create genebody_coverage.sh

---
+++ FigureS3/A/genebody_coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+bw="../../data/bw/*bw"
+annotation="../../data/mouse.bed"
+output="mouse_brain.readProfileMatrix.tsv.gz"
+out_plot="mouse_brain.readProfileMatrix.density.png" 
+
+echo "Compute Matrix..."
+computeMatrix scale-regions -S ${bw[@]} -R $annotation -o $output --upstream 1000 --downstream 1000 --sortRegions ascend --missingDataAsZero --skipZeros --metagene -p 6 
+
+echo "Plot Profile..."
+plotProfile -m $output -o $out_plot --perGroup --plotType se --yAxisLabel "mean CPM" --regionsLabel '' --plotTitle "Brain"
