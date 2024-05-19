beb0a633dbb877f42869e7f8363d928fb6fcd5d8
TamaraPerteghella <25196661+TamaraPerteghella@users.noreply.github.com>
2024-05-19 15:21:20 +0200

    Update getPolyAprofile.sh

--- FigureS2/C/getPolyAprofile.sh
+++ FigureS2/C/getPolyAprofile.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+while read lab cap tmp sample
+do
+  cat ../../data/bed/${lab}_${cap}_${tmp}_${sample}.bed | sortbed | bedtools slop -s -l 5 -r 5 -i stdin -g chromLengthInfo_hg38.txt | bedtools intersect -u -s -a stdin -b hg38.polyAsignals.bed > ${lab}_${cap}_${tmp}_${sample}.vspolyAsignals.bed
+
+  ...
+  TO DO
